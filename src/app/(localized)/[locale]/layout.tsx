fe079b6154ced1bcce92108f8911f9d0c5abc4fb
glodinasflexwork <angles.readier.7d@icloud.com>
2025-05-25 06:05:40 -0400

    Fix JSON parse error, NextIntlClientProvider implementation, and language switcher issues

--- src/app/(localized)/[locale]/layout.tsx
+++ src/app/(localized)/[locale]/layout.tsx
@@ -1,23 +1,31 @@
+'use client';
+
 import React from 'react';
 import { NextIntlClientProvider } from 'next-intl';
 import LocalizedHeader from '../../../components/LocalizedHeader';
 import LocalizedFooter from '../../../components/LocalizedFooter';
+import { getMessages } from '@/i18n/next-intl/getMessages';
 
 // Layout component for localized pages
-export default function LocalizedLayout({
+export default async function LocalizedLayout({
   children,
   params: { locale }
 }: {
   children: React.ReactNode;
   params: { locale: string };
 }) {
+  // Get messages for the current locale
+  const messages = await getMessages(locale);
+  
   return (
-    <div className="flex flex-col min-h-screen">
-      <LocalizedHeader />
-      <main className="flex-grow">
-        {children}
-      </main>
-      <LocalizedFooter />
-    </div>
+    <NextIntlClientProvider locale={locale} messages={messages}>
+      <div className="flex flex-col min-h-screen">
+        <LocalizedHeader />
+        <main className="flex-grow">
+          {children}
+        </main>
+        <LocalizedFooter />
+      </div>
+    </NextIntlClientProvider>
   );
 }
