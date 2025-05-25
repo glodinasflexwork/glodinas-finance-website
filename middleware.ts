51cf9e3cfc755843dff290fdb466ec0246e4b3fe
glodinasflexwork <angles.readier.7d@icloud.com>
2025-05-25 06:10:01 -0400

    Update middleware to apply to all pages, not just Getting Started

--- middleware.ts
+++ middleware.ts
@@ -7,20 +7,12 @@ export default createMiddleware({
   locales,
   // The default locale to use when visiting a non-localized route
   defaultLocale,
-  // Only apply to the Getting Started page as a proof of concept
-  pathnames: {
-    '/getting-started': {
-      en: '/getting-started',
-      nl: '/aan-de-slag',
-      ro: '/primii-pasi',
-      tr: '/baslangic',
-      ru: '/nachalo-raboty'
-    }
-  }
+  // Apply to all pages on the site
+  localePrefix: 'always'
 });
 
-// Configure middleware matcher to only apply to the Getting Started page
+// Configure middleware matcher to apply to all pages
 export const config = {
-  // Match only the localized routes for the Getting Started page
-  matcher: ['/(nl|en|ro|tr|ru)/getting-started', '/(nl|en|ro|tr|ru)/aan-de-slag', '/(nl|en|ro|tr|ru)/primii-pasi', '/(nl|en|ro|tr|ru)/baslangic', '/(nl|en|ro|tr|ru)/nachalo-raboty']
+  // Match all routes that start with a locale prefix
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
 };
